9576b2d792fef3268e1e2ff5275a4ed6aac9a7db
cjmario89 <mario89cheng@gmail.com>
2025-05-05 13:11:36 +0800

    feat: Enable HTTPS support in the application by configuring secure connection options with SSL certificates for enhanced security

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
+
+const httpsOptions = {
+  key: fs.readFileSync('./server.key'),
+  cert: fs.readFileSync('./server.cert'),
+};
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+  });
   await app.listen(3003);
 }
 bootstrap();
